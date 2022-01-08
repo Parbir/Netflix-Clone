c4a3685d2d603708e081414ba62216a7d9d9f6c3
Parbir <parbir.lehal@gmail.com>
2022-01-08 03:22:28 -0700

    added trailer functionality when movie is clicked

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import axios from "./axios"
 import "./Row.css"
+import YouTube from "react-youtube";
+import movieTrailer from 'movie-trailer';
+
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
     const base_url = "https://image.tmdb.org/t/p/original/";
     
@@ -16,7 +20,28 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
 
         fetchData();
 
-    }, [fetchUrl])
+    }, [fetchUrl]);
+
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        },
+    };
+
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl("");
+        } else {
+            movieTrailer(movie?.name || "")
+            .then((url) => {
+                const urlParams = new URLSearchParams(new URL (url).search);
+                setTrailerUrl(urlParams.get("v"));
+            })
+            .catch((error) => console.log(error));
+        }
+    }
 
     
     return (
@@ -30,10 +55,12 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
                 <img 
                 className={`row_poster ${isLargeRow && "row_posterLarge"}`}
                 key={movie.id}
+                onClick={() => handleClick(movie)}
                 src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
                 )
             ))}
             </div>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     );
 }
